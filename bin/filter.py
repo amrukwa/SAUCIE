415e41e918cd5c48372f28cb05ba639e18940cd5
amrukwa <a.mrukwa00@gmail.com>
2022-12-07 22:11:24 +0100

    chnage logs

--- bin/filter.py
+++ bin/filter.py
@@ -19,8 +19,8 @@ def filter_data(data, part=0.2):
     data = data.dropna(axis=1)
     data = data.loc[~(data == 0).all(axis=1)]
     data.drop(columns=data.columns[data.dtypes == object], inplace=True)
-    experiment.log_metrics(data_min=data.min())
-    experiment.log_metrics(data_max=data.max())
+    # experiment.log_metrics(data_min=data.min())
+    # experiment.log_metrics(data_max=data.max())
     x = np.var(data, axis=1)
     thr = (-1)*np.sort(-x)[int(x.shape[0]*part)]
     experiment.log_metrics(thr=thr)
